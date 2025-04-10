d81e1efac6d1c90df4828385d4b23eb3f8849109
Itzel Annet <itzelmunozlove@gmail.com>
2025-04-09 23:14:11 -0600

    Se sube el documento con los ejemplos faltantes

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ let encabezado2 = document.getElementById("encabezado2");
 // console.log(encabezado1.innerText);
 
 let listas = document.getElementsByTagName("ul");
+let txtRFC = document.getElementById("txtRFC");
+let txtCURP = document.getElementById("txtCURP");
+let txtTelefono = document.getElementById("txtTelefono");
 
 let elementos = document.getElementsByClassName("list-group-item");
 
@@ -48,9 +51,51 @@ btnMostrar.addEventListener("click", function (event) {
     //listas.item(0).before(element);
     // prepent: inserta el elemento al principio de la lista
     //listas.item(0).prepend(element2);
+    
     // append: inserta el elemento al final de la lista
-    listas.item(0).append(element);
+    //listas.item(0).append(element);
     // after: inserta el elemento despues de la lisa
-    listas.item(0).after(element2);
+    //listas.item(0).after(element2);
 
+    // afterbegin: inserta el elemento al principio de la lista
+    //listas.item(1).insertAdjacentElement("afterbegin", element);
+    // beforeend: inserta el elemento al final de la lista
+    //listas.item(1).insertAdjacentElement("beforeend", element2);
+
+    listas.item(1).insertAdjacentHTML("afterbegin", `<li class="list-group-item">Before begin item</li>`);
+    
+    listas.item(1).insertAdjacentHTML("afterend", `<li class="list-group-item">After end item</li>`);
+    
+    listas.item(1).insertAdjacentHTML("afterbegin", `<li class="list-group-item">After begin item</li>`);
+
+    listas.item(1).insertAdjacentHTML("beforeend", `<li class="list-group-item">Before end item</li>`);
+
+});
+
+// Se ejecuta cuando termina de cargar todos los elelemntos de la pagina
+window.addEventListener("load", function(event){
+    console.log("Se termino de cargar la pagina");
+    
+}); //load
+
+function txtToUpper(event){
+    event.target.value = event.target.value.trim().toUpperCase();
+}
+
+// blur -> cuando se sale del campo
+//txtRFC.addEventListener("blur", function(event){
+//    event.preventDefault();
+    //txtRFC.value = txtRFC.value.toUpperCase();
+//    event.value
+//}); //txtRFC
+// txtCURP.addEventListener("blur", function(event){
+//     event.preventDefault();
+//     txtCURP.value = txtCURP.value.toUpperCase();
+// })
+txtRFC.addEventListener("blur", txtToUpper); //txtRFC
+txtCURP.addEventListener("blur", txtToUpper); //txtCURP
+
+txtTelefono.addEventListener("", function(event){
+    event.preventDefault();
+    txtTelefono.value = txtTelefono.value.trim().slice(0, 10);
 });
